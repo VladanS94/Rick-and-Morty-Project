a52feb1cb138cd30cf1ae30256175f3cbd7261f9
VladanS10 <vladan.nish@gmail.com>
2024-10-11 02:48:12 +0200

    add dotenv

---
+++ react-app-env.d.ts
@@ -0,0 +1,9 @@
+interface ProcessEnv {
+  readonly REACT_APP_API_KEY: string;
+  readonly REACT_APP_AUTH_DOMAIN: string;
+  readonly REACT_APP_PROJECT_ID: string;
+  readonly REACT_APP_STORAGE_BUCKET: string;
+  readonly REACT_APP_MESSAGING_SENDER_ID: string;
+  readonly REACT_APP_APP_ID: string;
+  readonly REACT_APP_MEASUREMENT_ID: string;
+}
--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8ZZb5TXEo6Qscj4ki8E5MKgS9MOXZSuo",
-  authDomain: "shindiri-test.firebaseapp.com",
-  projectId: "shindiri-test",
-  storageBucket: "shindiri-test.appspot.com",
-  messagingSenderId: "52617965392",
-  appId: "1:52617965392:web:fc3ff399c4d2e6db6e4655",
-  measurementId: "G-CBB903KCR9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
